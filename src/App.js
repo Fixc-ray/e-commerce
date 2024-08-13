7cbf1b7489b6485310aa45d242c3d67571aa4b1f
Margaret Mwangi <margaret.mwangi3@moringaschool.com>
2024-08-13 13:02:43 +0300

    Modified the searchBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
+import Search from './components/Search';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  
 }
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useEffect , useState } from 'react';
 
 function Search() {
-  return (
-    <div>Search</div>
-  )
-}
+  let productData=[];
+
+  const [searchTerm , setSearchTerm] =useState('');
+  
+
+  const filteredProducts = productData.filter(product=>
+    product.name.toLowerCase().includes(searchTerm)|| product.category.toLowerCase().includes(searchTerm) ||
+    product.description.toLowerCase().includes(searchTerm)
+  );
+    return(
+      <div>
+      <input className="search-input" type="text" placeholder="DISCOVER PRODUCTS..." value={searchTerm}
+       />
+       <div>
+       {filteredProducts.map(product => (
+          <div key={product.id}>
+            <h2>{product.name}</h2>
+            <p>{product.category}</p>
+            <p>{product.description}</p>
+            </div>
+       ))}
+       </div>
+      </div>
+    );
+      }
 
 export default Search
