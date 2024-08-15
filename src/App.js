c7d79387f4128f2728838f67c5388f6b2ede1d1a
Bridget <bridget.njoki@student.moringaschool.com>
2024-08-15 10:37:52 +0300

    Added ultrafilter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Ultrafilter from './components/Ultrafilter';
 
 function App() {
   return (
     <div className="App">
 
       <Navbar />
+      <Ultrafilter/>
       <Home/>
+      
     </div>
   );
 }
