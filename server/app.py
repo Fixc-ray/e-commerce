fc42e4383cb55b30284832fead347d7f4326b1b4
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-10-17 17:33:13 +0300

    Initialized the User database

---
+++ server/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_migrate import Migrate
+
+from models import db, User
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user_data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+
+@app.route("/")
+def index():
+    return "<h1>Welcome to TasteNShop !</h1>"
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
---
+++ server/models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    @validates('email')
+    def validate_email(self, key, email):
+        if "@" or "." not in email:
+            raise ValueError("Invalid email address")
+        return email
+    
+    @validates('username')
+    def validate_username(self, key, username):
+        if len(username) == 0:
+            raise ValueError("Username cannot be empty")
+        return username
+
+    def __repr__(self):
+        return f'<User {self.username}>'
